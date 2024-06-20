6f155a1596a68335918bd14f0b9149ddd5bff00c
星星魔法术 <xingxingmofashu@outlook.com>
2024-06-20 17:31:09 +0800

    Update 1.CustomGeneratePalette.vue

--- packages/dashboard/components/antv/s2/theme/custom/1.CustomGeneratePalette.vue
+++ packages/dashboard/components/antv/s2/theme/custom/1.CustomGeneratePalette.vue
@@ -4,22 +4,11 @@
       <div class="text-center text-sm font-semibold">
         主题色自动生成色板
       </div>
-      <UPopover
-        mode="hover"
-        :popper="{ strategy: 'absolute' }"
-      >
+      <UPopover mode="hover" :popper="{ strategy: 'absolute' }">
         <template #default="{ open }">
-          <UButton
-            variant="ghost"
-            square
-            :class="[open && 'bg-gray-50 dark:bg-gray-800']"
-            aria-label="Color picker"
-          >
-            <UIcon
-              name="i-heroicons-swatch-20-solid"
-              class="w-5 h-5"
-              :style="{ color: (colors as any).hex }"
-            />
+          <UButton variant="ghost" square :class="[open && 'bg-gray-50 dark:bg-gray-800']" aria-label="Color picker">
+            <UIcon name="i-heroicons-swatch-20-solid" class="w-5 h-5"
+              :style="{ color: typeof colors === 'string' ? colors : (colors as any).hex }" />
           </UButton>
         </template>
         <template #panel>
