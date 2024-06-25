efedf44209bbe7fbb18bb61f469ff36b3aad81e3
星星魔法术 <148429905+xingxingmofashu@users.noreply.github.com>
2024-06-25 10:49:41 +0800

    migrate dashboard to a new repo (#12)

--- packages/dashboard/app.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <NuxtLoadingIndicator />
-    <NuxtLayout>
-      <NuxtPage page-key="dashboard" />
-    </NuxtLayout>
-  </div>
-</template>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    Dashboard Index
+    XCodenix
   </div>
 </template>
